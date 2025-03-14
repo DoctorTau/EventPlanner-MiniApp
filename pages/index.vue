a6adfab6e8eb0dc2479cffecda36e86a9383697a
DrTau <drttau@gmail.com>
2025-03-14 14:25:14 +0300

    Create some home page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to EventPlanner MiniApp</h1>
+    <p>This is the first page of your Nuxt.js application.</p>
+  </div></template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+}
+</style>
