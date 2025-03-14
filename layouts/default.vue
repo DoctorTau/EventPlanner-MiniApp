f093089c491b3d8912991db54c338d3ed1004575
DrTau <drttau@gmail.com>
2025-03-14 19:30:27 +0300

    Add telegram api

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+    title: 'EventPlanner MiniApp',
+    script: [{ src: 'https://telegram.org/js/telegram-web-app.js', defer: true }],
+})
+
+</script>
+
+<template>
+    <main>
+        <ClientOnly>
+            <slot />
+        </ClientOnly>
+    </main>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  vite: {
+    server: {
+      strictPort: false,
+      allowedHosts: ['.ngrok-free.app']
+    }
+  },
+
   modules: [
     '@nuxt/test-utils',
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/ui'
+    '@nuxt/ui',
   ]
 })
